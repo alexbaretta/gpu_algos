0ecbe15ad9689440d529ac511779d381724ac93c
Alex Baretta <alex@baretta.com>
2025-06-04 13:07:30 -0700

    common/benchmark_options

---
+++ include/common/benchmark_options.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2025 Alessandro Baretta
+// All rights reserved.
+
+// source path: include/common/benchmark_options.h
+
+#pragma once
+
+#include "common/benchmark_options.h"
+
+void add_benchmark_options(cxxopts::Options& options);
---
+++ src/common/benchmark_options.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2025 Alessandro Baretta
+// All rights reserved.
+
+// source path: src/common/benchmark_options.cpp
+
+#include "common/benchmark_options.h"
+
+void add_benchmark_options(cxxopts::Options& options) {
+    options.add_options()
+        ("i,init-method", "How to generate data: random, increasing, decreasing", cxxopts::value<std::string>()->default_value("random"))
+        ("gpumem", "GPU memory size", cxxopts::value<long>()->default_value(std::to_string(DEFAULT_GPU_MEM)))
+        ("seed", "Random seed", cxxopts::value<long>()->default_value(std::to_string(DEFAULT_SEED)))
+        ("errors", "Display errors", cxxopts::value<bool>()->default_value("false"))
+        ("verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"))
+        ("force,f", "Allow verbose output even if the input matrix is large", cxxopts::value<bool>()->default_value("false"))
+        ("h,help", "Print usage");
+}
