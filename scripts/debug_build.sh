b887e38074fb38ca9b67391048301286ac6e5135
Alex Baretta <alex@baretta.com>
2025-07-23 14:58:08 -0700

    build scripts: detect project_root dir

--- scripts/debug_build.sh
+++ scripts/debug_build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Copyright (c) 2025 Alessandro Baretta
+# All rights reserved.
+
+# source path: scripts/debug_build.sh
+
+set -euxo pipefail
+
+project_root="$(dirname ${0})/.."
+cd "$(readlink -f ${project_root})"
+
 cmake --build --preset debug -j $(nproc) "$@"
--- scripts/release_build.sh
+++ scripts/release_build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# Copyright (c) 2025 Alessandro Baretta
+# All rights reserved.
+
+# source path: scripts/release_build.sh
+
+set -euxo pipefail
+
+project_root="$(dirname ${0})/.."
+cd "$(readlink -f ${project_root})"
+
 cmake --build --preset release -j $(nproc) "$@"
